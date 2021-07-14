b2502620b06f547e066e80595b633f7634669708
DAnomaly <parksh1219@naver.com>
2021-07-14 16:49:27 +0900

    [210714] After Marging

--- workspace/shoefly/src/main/java/com/koreait/shoefly/controller/NoticeController.java
+++ workspace/shoefly/src/main/java/com/koreait/shoefly/controller/NoticeController.java
@@ -1,9 +1,15 @@
 package com.koreait.shoefly.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.koreait.shoefly.command.notice.SelectListFaqNoticeCommand;
+import com.koreait.shoefly.command.notice.SelectListNoticeNoticeCommand;
 
 import lombok.AllArgsConstructor;
 
@@ -13,14 +19,24 @@ import lombok.AllArgsConstructor;
 public class NoticeController {
 
 	private SqlSession sqlSession;
+	private SelectListNoticeNoticeCommand selectListNoticeNoticeCommand;
+	private SelectListFaqNoticeCommand selectListFaqNoticeCommand;
 	
 	@GetMapping("noticeListPage.do")
-	public String noticeListPage() {
+	public String noticeListPage(
+			Model model,
+			HttpServletRequest request) {
+		model.addAttribute("request", request);
+		selectListNoticeNoticeCommand.execute(sqlSession, model);
 		return "notice/noticeList";
 	}
 	
 	@GetMapping("faqListPage.do")
-	public String faqListPage() {
+	public String faqListPage(
+			Model model,
+			HttpServletRequest request) {
+		model.addAttribute("request", request);
+		selectListFaqNoticeCommand.execute(sqlSession, model);
 		return "notice/faqList";
 	}
 	
